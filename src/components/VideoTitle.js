7c0ce3a1736fcf54363723908018d9e3cd951ba4
jagannatarajan1 <jagannatarajan1@gmail.com>
2024-12-17 15:13:44 +0530

    fixed the screen

--- src/components/VideoTitle.js
+++ src/components/VideoTitle.js
@@ -3,7 +3,7 @@ import React from "react";
 const VideoTitle = ({ title, overview }) => {
   return (
     <>
-      <div className=" absolute pt-[20%] px-10   bg-gradient-to-r from-black ">
+      <div className=" absolute pt-[20%] px-10   bg-gradient-to-r from-black aspect-video ">
         <h1 className="text-4xl lg:text-5xl font-bold text-white ">{title}</h1>
         <p className="py-6 text-sm w-11/12 lg:text-lg lg:w-1/4 text-white ">
           {overview}
--- src/components/Videocontainer.js
+++ src/components/Videocontainer.js
@@ -21,7 +21,7 @@ const Videocontainer = () => {
   }, []);
 
   return (
-    <div className="">
+    <div className="w-full ">
       <iframe
         className="w-full aspect-video"
         src={
@@ -30,7 +30,6 @@ const Videocontainer = () => {
           "?autoplay=1&mute=1"
         }
         title="YouTube video player"
-        frameborder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
       ></iframe>
     </div>
